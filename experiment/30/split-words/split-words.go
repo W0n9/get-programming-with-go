1263ae7e87f66b35c238514acaf0c0afdd52c71a
w0n9 <38560218+W0n9@users.noreply.github.com>
2021-07-24 17:17:00 +0800

    30 milestone

--- experiment/30/split-words/split-words.go
+++ experiment/30/split-words/split-words.go
@@ -1,5 +1,36 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"strings"
+)
 
+func source(downstream chan string) {
+	for _, v := range []string{"foo bar", "bar baz", "foo baz"} {
+		downstream <- v
+	}
+	close(downstream)
+}
+
+func split(upstream, downstream chan string) {
+	for v := range upstream {
+		for _, j := range strings.Fields(v) {
+			downstream <- j
+		}
+	}
+	close(downstream)
+}
+
+func printout(upstream chan string) {
+	for v := range upstream {
+		fmt.Println(v)
+	}
+}
+
+func main() {
+	c1 := make(chan string)
+	c2 := make(chan string)
+	go source(c1)
+	go split(c1, c2)
+	printout(c2)
 }
